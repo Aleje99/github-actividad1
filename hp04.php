0e563edc6a8cd7255c3c221f4da7fc60acef633f
AlexBlu22 <ag.alejandro.230@gmail.com>
2020-11-20 02:37:15 +0100

    hp04_subida1

---
+++ hp04.php
@@ -0,0 +1,11 @@
+<html>
+ <head>
+  <title>Harry Porlotes y El Malware de Fuego</title>
+ </head>
+ <body>
+<?php echo '<h1>Harry Porlotes y El Malware de Fuego</h1>'; ?>
+<?php print("<p>Un importante hackaton se celebra en la escuela y el portátil de un compañero de Harry, se avería debido a un ataque de Ransomwaremort, perdiendo así toda la saga Crepúsculo en epub que tenía guardada y las revistas de Marie-Claire de su abuela en PDF.</p>"
+           );?>
+<?php print("<p>Pero loco, que me cuentas tu aqui, jajaja, XD, LOL, me encanta el fornike?</p>");?>
+ </body>
+</html>
